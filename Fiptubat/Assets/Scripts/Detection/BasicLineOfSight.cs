21e17c80bac3c94708d86641456bae4b05af6d71
aceade <philiprowlands90@gmail.com>
2019-10-05 09:48:38 +0100

    #5 adding basic detection interface

---
+++ Fiptubat/Assets/Scripts/Detection/BasicLineOfSight.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Aceade.AI {
+
+	/// <summary>
+	/// A basic line of sight implementation.
+	/// </summary>
+	public class BasicLineOfSight : MonoBehaviour, IDetection {
+
+		public float detectionInterval = 0.2f;
+		WaitForSeconds delay;
+
+
+		// Use this for initialization
+		void Start () 
+		{
+			delay = new WaitForSeconds (detectionInterval);
+		}
+
+		public void OnTriggerEnter(Collider coll) 
+		{
+			
+		}
+
+		public void OnTriggerExit(Collider coll)
+		{
+		}
+
+		public IEnumerator ProcessColliders()
+		{
+			yield return delay;	
+		}
+	}
+}
---
+++ Fiptubat/Assets/Scripts/Detection/DetectionBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Aceade.AI {
+	
+	/// <summary>
+	/// Defines how detection will work
+	/// </summary>
+	public interface IDetection {
+
+		void OnTriggerEnter(Collider coll);
+
+		void OnTriggerExit(Collider coll);
+
+		IEnumerator ProcessColliders();
+	}
+
+}
