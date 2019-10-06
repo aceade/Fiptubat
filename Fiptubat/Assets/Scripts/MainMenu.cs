f734578c6a29e22409697629695b40d204ae4466
aceade <philiprowlands90@gmail.com>
2019-10-06 16:14:54 +0100

    #10 adding release and main menu

---
+++ Fiptubat/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Attached to the camera in the main menu.
+/// <summary>
+public class MainMenu : MonoBehaviour
+{
+    public string sceneName;
+
+    public void StartTheGame() {
+        SceneManager.LoadScene(sceneName);
+    }
+}
