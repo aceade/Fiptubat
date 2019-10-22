12d286f85d840e96210f2fdbf5b9423dc11d3cb6
aceade <philiprowlands90@gmail.com>
2019-10-22 20:10:12 +0100

    #18 adding FireMode

---
+++ Fiptubat/Assets/Scripts/Combat/FireMode.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A specific firing mode, e.g. Snap Shot, Aimed Shot, Short Burst, etc.
+/// <summary>
+[CreateAssetMenu(fileName = "FireMode", menuName = "Aceade/Combat/FireMode", order = 1)]
+public class FireMode : ScriptableObject
+{
+    public string description;
+
+    [Tooltip("How much the shot will deviate from right-on-target")]
+    public float deviation = 0.01f;
+
+    [Tooltip("How much does this affect the shooting cost")]
+    public float modifier = 1f;
+}
