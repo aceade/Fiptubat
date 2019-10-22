3a2ab1284e343a35a78fe06ed860404e6f0a2140
aceade <philiprowlands90@gmail.com>
2019-10-22 20:40:24 +0100

    #18 adding another fire mode, weapon base

---
+++ Fiptubat/Assets/Scripts/Combat/WeaponBase.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponBase : MonoBehaviour
+{
+    public int damage;
+
+    public int baseCost = 5;
+
+    public List<FireMode> fireModes;
+
+    private int currentFireMode;
+
+    private Transform muzzle;
+
+    void Start()
+    {
+        muzzle = transform;
+    }
+
+    /// <summary>
+    /// FIIIIIIRE!
+    /// </summary>
+    /// <return>
+    ///     Return true if it hit the target.
+    /// </return>
+    public bool Fire() {
+        return false;
+    }
+
+    public FireMode GetCurrentFireMode() {
+        return fireModes[currentFireMode];
+    }
+
+    public void CycleFireMode() {
+        currentFireMode++;
+        if (currentFireMode >= fireModes.Count) {
+            currentFireMode = 0;
+        }
+    }
+
+    public void SelectFireMode (int fireMode) {
+        if (fireMode >= fireModes.Count) {
+            fireMode = 0;
+        }
+        currentFireMode = fireMode;
+    }
+
+    public int GetCurrentFireCost() {
+        return Mathf.RoundToInt(GetCurrentFireMode().modifier * baseCost);
+    }
+}
