aa9c8f29cb5ea6de1eec2084a06f6975a8bfd0d1
aceade <philiprowlands90@gmail.com>
2020-01-02 19:06:51 +0000

    Basic models for main level

---
+++ Fiptubat/Assets/Scripts/Combat/DummyDamage.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyDamage : MonoBehaviour, IDamage
+{
+    public int health = 1;
+
+    void Start() {
+        Debug.LogFormat("DummyDamage {0} online", this);
+        GetComponent<Rigidbody>().isKinematic = false;
+        GetComponent<Rigidbody>().WakeUp();
+    }
+
+    public Transform GetTransform() {
+        return transform;
+    }
+
+    public int GetRemainingHealth() {
+        return 0;
+    }
+
+    public void Damage(DamageType damageType, int damageAmount) {
+        Debug.LogFormat("{0} took {1} points of {2} damage", this, damageAmount, damageType);
+        if (damageAmount > health) {
+            Die();
+        }
+    }
+
+    private void Die() {
+        Debug.LogFormat("{0} is deaded!!!1!", this);
+    }
+
+    public int GetPotentialDamage() {
+        return 0;
+    }
+}
---
+++ Fiptubat/Assets/Scripts/FallDeathTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Deals fatal damage to anything that enters it. Purely in case some idiot walks off a cliff.
+/// </summary>
+public class FallDeathTrigger : MonoBehaviour
+{
+    private int damage = 20000000;
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider collider)
+    {
+        if (!collider.isTrigger) {
+            var damageScript = collider.transform.root.GetComponent<IDamage>();
+            damageScript.Damage(DamageType.REGULAR, damage);
+        }
+    }
+}
