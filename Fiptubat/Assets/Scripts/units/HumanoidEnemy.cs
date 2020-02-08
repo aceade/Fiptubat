5d4185c316c0b7c34d98bd152b1ccc892816e453
aceade <philiprowlands90@gmail.com>
2020-02-08 11:02:10 +0000

    #53 adding initial class for humanoids

---
+++ Fiptubat/Assets/Scripts/units/HumanoidEnemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Humanoid enemies. Can do the following:
+/// * Stand guard if they have no patrol points and will patrol otherwise
+/// * Move towards an enemy if they can't see it.
+/// * If they can see an enemy, they will move to cover and then remain there
+/// </summary>
+public class HumanoidEnemy : BaseUnit
+{
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+}
